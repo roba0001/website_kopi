638ff0737eccdc5fddaa4e5afbf082006bfec4fc
Ronja <roba0001@stud.kea.dk>
2024-01-09 23:02:09 +0100

    HELT DONE

--- menu.js
+++ menu.js
@@ -2,7 +2,9 @@
 const burger = document.querySelector(".burger");
 const nav = document.querySelector("nav");
 const menu = document.querySelector(".menu");
+const menu_index = document.querySelector("#menu_index");
 const links = document.querySelectorAll(".menu li a");
+const links_index = document.querySelectorAll("#menu_index li a");
 
 burger.addEventListener("click", () => {
   burger.classList.toggle("active");
